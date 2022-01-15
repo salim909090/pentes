c4fc32887560e78076a826b805595d9dfa28ccc7
salim909090 <58624725+salim909090@users.noreply.github.com>
2022-01-15 15:13:53 +0000

    Update testfile.php

--- testfile.php
+++ testfile.php
@@ -1,1 +1,11 @@
-<?PHP echo "Hello THM"; ?>
+<?PHP
+echo "Hello THM";
+function debug_to_console($data) {
+    $output = $data;
+    if (is_array($output))
+        $output = implode(',', $output);
+
+    echo "<script>console.log('Debug Objects: " . $output . "' );</script>";
+}
+debug_to_console("Test");
+?>
